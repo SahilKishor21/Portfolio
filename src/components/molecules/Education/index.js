251521c4a8b9c6f65986ae6cbd6464ac933096fb
SahilKishor21 <sahil.21ug4021@iiitranchi.ac.in>
2024-12-21 07:06:02 +0530

    kupdated About

---
+++ src/components/molecules/Education/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Lottie from "react-lottie";
+import readingBook from "../../../assets/reading-book.json";
+
+const Education = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: readingBook,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div className="pt-24">
+      <div className="mb-12">
+        <h1 className="text-4xl font-semibold drop-shadow-md text-center">
+          My <span className="text-primary">Qualification</span>
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8">
+        <div className="">
+          <Lottie
+            options={defaultOptions}
+            height="70%"
+            width="90%"
+            className="mx-auto lg:mr-auto"
+          />
+        </div>
+        <div className="mx-auto lg:ml-auto">
+          {/* Diploma */}
+          <div className="p-6 bg-[#313131] bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg inline-block w-[300px] hover:shadow-primary duration-300 cursor-pointer">
+            <div className="text-right mb-4">
+              <h3 className="text-2xl font-semibold text-primary">
+              Indian Institute of Information Technology, Ranchi
+              </h3>
+              <p className="text-sm text-neutral font-semibold">2021 - 2025</p>
+            </div>
+            <p className="text-sm text-neutral text-justify">
+              I am currently pursuing my Bachelor's Degree in Electronics and Communication Engineering with specialization in Embedded Systems and Internet of Things.
+            </p>
+          </div>
+
+          {/* React Developer */}
+          <div className="my-6 md:my-4 md:ml-[200px] p-6 bg-[#313131] bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg w-[300px] hover:shadow-primary duration-300 cursor-pointer">
+            <div className="text-right mb-4">
+              <h3 className="text-2xl font-semibold text-primary">
+              St.Joseph High School, Patna
+              </h3>
+              <p className="text-sm text-neutral font-semibold">2020</p>
+            </div>
+            <p className="text-sm text-neutral text-justify">
+              I have completed my 12th grade with major subjects as Physics, Chemistry, Mathematics and Computer Science.
+            </p>
+          </div>
+
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
---
+++ src/pages/About/index.js
@@ -0,0 +1,133 @@
+import React from "react";
+import TypeAnimation from "react-type-animation";
+import { motion } from "framer-motion";
+import "./About.css";
+import "../../components/atoms/PrimaryBtn/PrimaryBtn.css";
+import "../shared/Shared.css";
+import { BottomLine, MySkill, Education } from "../../components";
+import { FaDownload } from "react-icons/fa";
+
+const About = () => {
+  return (
+    <div className="parent pt-16 my-16">
+      <div className="">
+        <motion.div
+          className="mb-10"
+          initial={{ y: -200, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 1, type: "spring" },
+          }}
+        >
+          <h3 className="text-neutral text-center">Something About Myself</h3>
+          <h1 className="text-4xl font-semibold drop-shadow-md text-center">
+            About <span className="text-primary">Me</span>
+          </h1>
+          <BottomLine />
+        </motion.div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
+          <motion.div
+            initial={{ x: -200, opacity: 0 }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 1.25 },
+            }}
+          >
+            <img
+              src="https://res.cloudinary.com/djqkofsv0/image/upload/v1734737570/Me_tt0pxv.jpg"
+              alt="Sahil"
+              className="p-12 w-70 h-70 transform translate-y-[-12%]"
+              title="Sahil Kishor"
+            />
+
+          </motion.div>
+          <motion.div
+            className=""
+            initial={{ x: 200, opacity: 0 }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 1.25 },
+            }}
+          >
+            <h1 className="text-4xl font-semibold mb-4 text-center translate-y-[-380%] sm:translate-y-[-0%] sm:text-3xl sm:mb-2 md:text-left">Sahil Kishor</h1>
+            <div className="my-8">
+              <TypeAnimation
+                className="text-2xl text-primary font-bold text-center translate-y-[-500%] sm:translate-y-[-0%]  sm:text-2xl sm:mb-2 md:text-left"
+                cursor={true}
+                sequence={[
+                  "A Mern-stack Developer",
+                  2000,
+                  "A Full-stack Developer",
+                  2000,
+                  "A Front-end Developer",
+                  2000
+                ]}
+                wrapper="div"
+                repeat={Infinity}
+              />
+            </div>
+            <p className="text-neutral font-medium text-center translate-y-[-100%] sm:translate-y-[-0%] sm:mb-2 md:text-left">
+              As a MERN stack developer, I am committed to building high-quality
+              web applications that meet the needs of my clients. With years of
+              experience in full-stack web development, I specialize in using
+              React.js, Next js, MySQL, MongoDB, Express.js, and Node.js to
+              create scalable and robust web applications.
+              {/* Dedicated Front-end developer. Capable to solve working problems.
+              Passionate about learning & development to reach the target. Eager
+              to tackle more complex problems and continue to find ways to
+              maximize user efficiency. */}
+            </p>
+            <br />
+            <p className="text-neutral font-medium text-center translate-y-[-150%] sm:translate-y-[-0%] sm:mb-2 md:text-left">
+              My passion for solving complex problems and creating innovative
+              solutions drives me to stay up-to-date with the latest
+              technologies and trends in the industry.
+              {/* My next mission is how to become a Mern stack Developer. I would
+              love to build some Giant Website which will shine myself. */}
+            </p>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-4 mt-4 text-center translate-y-[-50%] sm:translate-y-[-0%] sm:mb-2 md:text-left ">
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Name : </span>Sahil Kishor
+              </h2>
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Phone : </span>+91 7562007896
+              </h2>
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Email : </span>
+                Sahilkishor8@gmail.com
+              </h2>
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Address : </span>Patna, Bihar
+              </h2>
+              <a
+                href="https://drive.google.com/file/d/1arBC3MZ_XDHOKUEWXJxEtZL0KVDq1Lir/view?usp=drivesdk"
+                target="blank"
+              >
+                <div className="flex justify-center md:justify-start translate-y-[60%] sm:translate-y-[-0%]">
+                  <button className="primary-button">
+                    <span>My Resume</span>
+                    <span>
+                      <FaDownload />
+                    </span>
+                  </button>
+                </div>
+
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+
+      {/* My Skill */}
+      <MySkill />
+
+      {/* Education */}
+      <Education />
+    </div>
+  );
+};
+
+export default About;
